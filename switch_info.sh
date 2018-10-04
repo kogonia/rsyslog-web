70bd25f41d2fce2fc23e7f85ba68fc8d7ce971b1
Георгий Когония <gio@TEH-N01.a-mobile.ab>
2018-10-04 11:24:57 +0300

    fix ifAlias with spaces in name

--- switch_info.sh
+++ switch_info.sh
@@ -14,18 +14,19 @@ ifAdminStatus=$(snmpwalk -v 2c -c $community $ip ifAdminStatus | cut -d' ' -f4 |
 arrAdminStatus=()
 IFS=',' read -r -a arrAdminStatus <<< "$ifAdminStatus"
 
-ifAlias=$(snmpwalk -v 2c -c $community $ip ifAlias | cut -d' ' -f4 | tr '\n' ',')
+ifAlias=$(snmpwalk -v 2c -c $community $ip ifAlias | grep -o ':\s.*' | grep -o '\s.*' | tr '\n' ',')
 arrAlias=()
 IFS=',' read -r -a arrAlias <<< "$ifAlias" 
 
 arr=()
 len=${#arrAlias[@]}
+arr+=("<tr allign='center'><td> <b>interface</b> </td><td> <b>status</b> </td><td> <b>description</b> </td></tr>")
 for (( c=0; c<$len; c++ ));
 do
-    arr+=("<td>" ${arrDescr[$c]} "</td><td>" ${arrAdminStatus[$c]} "</td><td>" ${arrAlias[$c]} "</td></tr><tr allign='center'>")
+    arr+=("<tr allign='center'><td>" ${arrDescr[$c]} "</td><td>" ${arrAdminStatus[$c]} "</td><td>" ${arrAlias[$c]} "</td></tr>")
 done
 echo -e "[ $ip ] <b>$sysName</b><br/>"
-echo -e "$sysDescr"
-echo -e "<table border='1' cellpadding='5' cellspacing='2'><tr allign='center'>"
+echo -e "${sysDescr//$'\n'/<br\/>}"
+echo -e "<table border='1' cellpadding='5'>"
 echo -e "${arr[@]}"
-echo -e "</tr></table>"
+echo -e "</table>"
